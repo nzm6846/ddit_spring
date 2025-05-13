df41eb3cd364d1bf919fc48699975636f6e22c74
nzm <nzm6846@gmail.com>
2025-05-13 10:23:38 +0900

    dto 만들기

---
+++ ddit/src/main/java/kr/or/ddit/dto/ArticleForm.java
@@ -0,0 +1,38 @@
+package kr.or.ddit.dto;
+
+//자바빈 클래스 : 자바빈 규약을 지키는 클래스
+//              1) 프로퍼티 2) 기본생성자 3) getter/setter메소드
+public class ArticleForm {
+    //제목을 받을 필드
+    private String title;
+    //내용을 받을 필드
+    private String content;
+    //기본생성자
+    public ArticleForm() {
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "ArticleForm{" +
+                "title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
