da90888b5102173803a2a916bb59676a4a82f597
nzm <nzm6846@gmail.com>
2025-05-15 10:50:11 +0900

    5/15 2교시

--- ddit/src/main/java/kr/or/ddit/controller/FirstController.java
+++ ddit/src/main/java/kr/or/ddit/controller/FirstController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
 import java.util.Optional;
 
 /*
@@ -130,7 +131,8 @@ public class FirstController {
     요청 방식 : get
      */
     @GetMapping("/articles/{id}")
-    public String show(@PathVariable(value="id") Long id){
+    public String show(@PathVariable(value="id") Long id,
+                       Model model){
         //매개변수로 id 받아오기
         //id를 잘 받았는지 확인하는 로그 찍기
         log.info("show->id : {}",id);
@@ -147,11 +149,35 @@ public class FirstController {
         log.info("show->articleEntity : "+articleEntity);
         //2. 모델에 데이터 등록하기
         //article이라는 이름으로 value인 articleEntity 객체 추가
+        model.addAttribute("article",articleEntity);
 
         //3. 뷰 페이지 반환하기
         // 뷰 페이지는 articles라는 디렉터리 안에 show라는 파일이 있다는 의미
         return "articles/show";
     }
 
+    /* 글 목록
+    요청URI : /articles
+    요청파라미터 :
+    요청방식 : get
+     */
+
+    @GetMapping("/articles")
+    public String index(Model model){
+        //1. 모든 데이터 가져오기
+        //findAll() 메서드의 반환 데이터 타입은 Iterable. List라서 불일치
+        //첫째, 캐스팅(형변환). Iteratable, Collection, LList 인터페이스의 상하 관계는
+        // Iteratable이 가장 상위 인터페이스
+        List<Article> articleEntityList = this.articleRepository.findAll();
+
+        //2. 모델에 데이터 등록하기(쌍쌍 쉼 쌍쌍)
+        model.addAttribute("articleList",articleEntityList);
+
+        //3. 뷰 페이지 설정하기
+        // articles 디렉터리 안에 index.mustache 파일이 뷰 페이지로 설정
+        //forwarding : mustache
+        return "articles/index";
+
+    }
 
 }
